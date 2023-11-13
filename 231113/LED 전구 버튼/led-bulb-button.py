098ec6e7b54b3de55f384ec2adb7cff823cb1302
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-11-13 17:41:45 +0900

    Title: LED 전구 버튼, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231113/LED 전구 버튼/led-bulb-button.py
+++ 231113/LED 전구 버튼/led-bulb-button.py
@@ -13,10 +13,22 @@ def press(arr):
             arr[i] = dic[arr[i]]
     return arr
 
+S.add(tuple(arr))
 for _ in range(B):
     arr = press(arr)
-    if 1 not in arr:
+    if tuple(arr) in S:
         break
+    else:
+        S.add(tuple(arr))
 
-for x in arr:
-    print(x)
+arr = origin
+for idx in range(B%len(S)):
+    arr = press(arr)
+
+if 1 not in arr:
+    for _ in range(len(arr)):
+        print(0)
+
+else:
+    for x in arr:
+        print(x)
