0c99d7e2588e197f2ad3b2f108e0c58282da6b1a
kys1124 <125456582+kys1124@users.noreply.github.com>
2023-11-13 16:54:49 +0900

    Title: 1로 이루어진 집합의 크기, Time: 121ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 231113/1로 이루어진 집합의 크기/size-of-set-of-1s.py
+++ 231113/1로 이루어진 집합의 크기/size-of-set-of-1s.py
@@ -2,21 +2,18 @@ N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
 v = [[0]*M for _ in range(N)]
-
-
-
 def dfs(si,sj,idx):
     q= [(si,sj)]
-    v[si][sj]=idx
+    v[si][sj] = idx
     cnt = 1
     while q:
         temp_q= []
-        for i in range(len(q)):
-            ci,cj =q[i]
+        for k in range(len(q)):
+            ci,cj = q[k]
             for di,dj in ((1,0),(-1,0),(0,1),(0,-1)):
                 ni,nj = ci+di,cj+dj
-                if 0<=ni<N and 0<nj<M and v[ni][nj]==0 and arr[ni][nj]==1:
-                    v[ni][nj]=idx
+                if 0<=ni<N and 0<=nj<M and v[ni][nj]==0 and arr[ni][nj]==1:
+                    v[ni][nj]= idx
                     temp_q.append((ni,nj))
                     cnt+=1
         q = temp_q
@@ -27,7 +24,8 @@ cnt_lst = [0]
 for i in range(N):
     for j in range(M):
         if arr[i][j]==1 and v[i][j]==0:
-            cnt = dfs(i,j,idx)
+            cnt = dfs(i,j, idx)
+            mx = max(mx, cnt)
             cnt_lst.append(cnt)
             idx+=1
 
@@ -41,6 +39,6 @@ for i in range(N):
                 if 0<=ni<N and 0<=nj<M and arr[ni][nj]==1:
                     S.add((v[ni][nj]))
             for num in S:
-                cnt+=cnt_lst[num]
+                cnt+= cnt_lst[num]
             mx = max(cnt,mx)
 print(mx)
